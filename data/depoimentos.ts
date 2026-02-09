0f0bd285b1759308042f1de4d0c26de95951f7b6
pedrosh101 <pedro.sh101@gmail.com>
2026-02-09 14:27:43 -0300

    AF148

--- data/depoimentos.ts
+++ data/depoimentos.ts
@@ -13,7 +13,7 @@ export const depoimentos: Depoimentos[] = [
   },
   {
     texto:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae nisi fringilla, bibendum lorem quis, convallis mi.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae nisi fringilla, bibendum lorem quis, convallis.",
     autor: "João Santos",
     curso: "Pai de aluno - Teatro Infantil",
   },
